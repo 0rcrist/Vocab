f0e9f07e45365667a7c1a40423b4da4223d75c74
Daniel Gibney <Dpgibney>
2018-07-19 19:42:04 -0400

    lyrics parser first upload

---
+++ lyrics.py
@@ -0,0 +1,114 @@
+class lyrics:
+
+    def __init__(self):
+        self.artists = {}
+
+    def add_song(self, artist, album, song, lyrics):
+        if artist not in self.artists:
+            self.artists[artist] = {}
+        if album not in self.artists[artist]:
+            self.artists[artist][album] = {}
+        if song not in self.artists[artist][album]:
+            self.artists[artist][album][song] = {}
+            for word in lyrics.split(" "):
+                if word.lower() not in self.artists[artist][album][song]:
+                    self.artists[artist][album][song][word.lower()] = 1
+                else:
+                    self.artists[artist][album][song][word.lower()] += 1
+
+    def get_artists(self):
+        tmp = []
+        for artist in self.artists:
+            tmp.append(artist)
+        return tmp
+
+    def remove_song(self, artist, album, song):
+        if artist in self.artists:
+            if album in self.artists[artist]:
+                if song in self.artists[artist][album]:
+                    del self.artists[artist][album][song]
+                else:
+                    raise ReferenceError('Song not found')
+            else:
+                raise ReferenceError('Album not found')
+        else:
+            raise ReferenceError('Artist not found')
+
+    def remove_album(self, artist, album):
+        if artist in self.artists:
+            if album in self.artists[artist]:
+                del self.artists[artist][album]
+            else:
+                raise ReferenceError('Album not found')
+        else:
+            raise ReferenceError('Artist not found')
+
+    def remove_artist(self, artist):
+        if artist in self.artists:
+            del self.artists[artist]
+        else:
+            raise ReferenceError('Album not found')
+
+    def empty(self):
+        self.artists = {}
+
+    def get_words_artist(self, artist):
+        tmp = {}
+        if artist in self.artists:
+            for albums in self.artists[artist]:
+                for songs in self.artists[artist][albums]:
+                    for words in self.artists[artist][albums][songs]:
+                        if words not in tmp:
+                            tmp[words] = self.artists[artist][albums][songs][words]
+                        else:
+                            tmp[words] += self.artists[artist][albums][songs][words]
+        else:
+            raise ReferenceError('Artist not found')
+        return tmp
+
+    def get_words_album(self, artist, album):
+        tmp = {}
+        if artist in self.artists:
+            if album in self.artists[artist]:
+                for songs in self.artists[artist][album]:
+                    for words in self.artists[artist][album][songs]:
+                        if words not in tmp:
+                            tmp[words] = self.artists[artist][album][songs][words]
+                        else:
+                            tmp[words] += self.artists[artist][album][songs][words]
+            else:
+                raise ReferenceError('Album not found')
+        else:
+            raise ReferenceError('Artist not found')
+        return tmp
+
+    def get_words_song(self, artist, album, song):
+        tmp = {}
+        if artist in self.artists:
+            if album in self.artists[artist]:
+                if song in self.artists[artist][album]:
+                    for words in self.artists[artist][album][song]:
+                        if words not in tmp:
+                            tmp[words] = self.artists[artist][album][song][words]
+                        else:
+                            tmp[words] += self.artists[artist][album][song][words]
+                else:
+                    raise ReferenceError('Song not found')
+            else:
+                raise ReferenceError('Album not found')
+        else:
+            raise ReferenceError('Artist not found')
+        return tmp
+
+# add overloaded functions to get the unique word count total word counts and unique words
+# maybe additionally overload a remove function
+# maybe also a print function? although that seems less worth while
+
+#if __name__ == "__main__":
+#    print('hello')
+#    test = lyrics()
+#    test.add_song("ehlaksdf", "asdf", "asdfsa", "alskfjd asdfa asfd asfd")
+#    print(test.artists)
+#    print(test.get_words_artist("ehlaksdf"))
+#    print(test.get_artists())
+#    #test.remove_song("asdf", "asdfasfd", "asdfasdf")
---
+++ testlyrics.py
@@ -0,0 +1,57 @@
+import unittest
+from lyrics import lyrics
+
+class Testlyrics(unittest.TestCase):
+    def test_add(self):
+        tmp = lyrics()
+        tmp.add_song("Toto", "Toto IV", "africa", "I hear the drums echoing tonight \
+But she hears only whispers of some quiet conversation \
+Shes coming in 12:30 flight ")
+        self.assertEqual(tmp.get_words_artist("Toto"), {'i': 1, 'hear': 1, 'the': 1, 'drums': 1, 'echoing': 1, 'tonight': 1, 'but': 1, 'she': 1, 'hears': 1, 'only': 1, 'whispers': 1, 'of': 1, 'some': 1, 'quiet': 1, 'conversation': 1, 'shes': 1, 'coming': 1, 'in': 1, '12:30': 1, 'flight': 1, '': 1} )
+
+    def test_get_artists(self):
+        tmp = lyrics()
+        tmp.add_song("Toto", "Toto IV", "africa", "I hear the drums")
+        tmp.add_song("Disturbed", "immortalized", "The Sound of Silence", "Hello darkness my old friend")
+        tmp.add_song("Eminem", "The Marshall Mathers LP 2", "Rap God", "Look I was gonna go easy on you and not to hurt your feelings")
+        self.assertEqual(tmp.get_artists(), ['Toto', 'Disturbed', 'Eminem'])
+
+
+
+    def test_exceptions(self):
+        tmp = lyrics()
+        tmp.add_song("Toto", "Toto IV", "africa", "I hear the drums")
+        tmp.add_song("Disturbed", "immortalized", "The Sound of Silence", "Hello darkness my old friend")
+        tmp.add_song("Eminem", "The Marshall Mathers LP 2", "Rap God", "Look I was gonna go easy on you and not to hurt your feelings")
+        with self.assertRaises(ReferenceError):
+            tmp.get_words_artist("Billy")
+        with self.assertRaises(ReferenceError):
+            tmp.get_words_album("Toto", "not real")
+        with self.assertRaises(ReferenceError):
+            tmp.get_words_song("Toto", "not real", "africa")
+        with self.assertRaises(ReferenceError):
+            tmp.get_words_song("Toto", "Toto IV", "not real")
+        with self.assertRaises(ReferenceError):
+            tmp.remove_artist("jimmy")
+        with self.assertRaises(ReferenceError):
+            tmp.remove_album("Toto", "Toto I")
+        with self.assertRaises(ReferenceError):
+            tmp.remove_song("Toto", "Toto IV", "nevada")
+
+    def test_removing(self):
+        tmp = lyrics()
+        tmp.add_song("Toto", "Toto IV", "africa", "I hear the drums")
+        tmp.add_song("Toto", "Toto IV", "nevada", "I hear the drums")
+        tmp.add_song("Disturbed", "immortalized", "The Sound of Silence", "Hello darkness my old friend")
+        tmp.add_song("Disturbed", "immortalized", "The Sound", "old friend")
+        tmp.add_song("Eminem", "The Marshall Mathers LP 2", "Rap God", "Look I was gonna go easy on you and not to hurt your feelings")
+        tmp.remove_song("Toto", "Toto IV", "africa")
+        self.assertEqual(tmp.artists,{'Toto': {'Toto IV': {'nevada': {'i': 1, 'hear': 1, 'the': 1, 'drums': 1}}}, 'Disturbed': {'immortalized': {'The Sound of Silence': {'hello': 1, 'darkness': 1, 'my': 1, 'old': 1, 'friend': 1}, 'The Sound': {'old': 1, 'friend': 1}}}, 'Eminem': {'The Marshall Mathers LP 2': {'Rap God': {'look': 1, 'i': 1, 'was': 1, 'gonna': 1, 'go': 1, 'easy': 1, 'on': 1, 'you': 1, 'and': 1, 'not': 1, 'to': 1, 'hurt': 1, 'your': 1, 'feelings': 1}}}})
+        tmp.remove_album("Disturbed", "immortalized")
+        self.assertEqual(tmp.artists,{'Toto': {'Toto IV': {'nevada': {'i': 1, 'hear': 1, 'the': 1, 'drums': 1}}}, 'Disturbed': {}, 'Eminem': {'The Marshall Mathers LP 2': {'Rap God': {'look': 1, 'i': 1, 'was': 1, 'gonna': 1, 'go': 1, 'easy': 1, 'on': 1, 'you': 1, 'and': 1, 'not': 1, 'to': 1, 'hurt': 1, 'your': 1, 'feelings': 1}}}})
+        tmp.remove_artist("Toto")
+        self.assertEqual(tmp.artists,{'Disturbed': {}, 'Eminem': {'The Marshall Mathers LP 2': {'Rap God': {'look': 1, 'i': 1, 'was': 1, 'gonna': 1, 'go': 1, 'easy': 1, 'on': 1, 'you': 1, 'and': 1, 'not': 1, 'to': 1, 'hurt': 1, 'your': 1, 'feelings': 1}}}} )
+        tmp.empty()
+        self.assertEqual(tmp.artists, {})
+
+
